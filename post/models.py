1d35eaff8daa6964b17b675ac267447f77499199
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-12 20:03:01 +0300

    update post/models.py
    -add related name & settings.AUTH_USER_MODEL

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 
+from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
@@ -16,7 +17,9 @@ class Post(models.Model):
     content = models.TextField()
     image = models.ImageField(null=True, upload_to=post_image_file_path)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name="posts")
 
     class Meta:
         ordering = ["-created_at"]
