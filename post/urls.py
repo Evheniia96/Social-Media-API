02754e2e935861d834770bc6b92260df2232f9fb
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-12 20:48:54 +0300

    add urls

---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from post.views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register("posts", PostViewSet, basename="post")
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "post"
