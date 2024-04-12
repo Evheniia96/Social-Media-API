b13f5ce8c167eca1128d028513bb7d23893f23e7
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-12 20:48:24 +0300

    add PostViewSet in the post/views.py

--- post/views.py
+++ post/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from post.models import Post
+from post.serializers import PostSerializer
 
 
+class PostViewSet(ModelViewSet):
+    """Post CRUD endpoints"""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        play = self.get_object()
+        serializer = self.get_serializer(play, data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
