6a92a36ec212e8185322d98ced0fad33d4e6d805
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-13 21:59:44 +0300

    add feature login and url: me/

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView, RetrieveUserView
+from user.views import CreateUserView, RetrieveUserView, ManageUserView, LoginUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("<int:pk>/", RetrieveUserView.as_view(), name="retrieve"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
