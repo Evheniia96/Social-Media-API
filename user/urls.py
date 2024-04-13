a0d6eecf61225063c6dffd17447c124874f86791
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-13 21:09:57 +0300

    add UserRetrieve and add url

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, RetrieveUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("<int:pk>/", RetrieveUserView.as_view(), name="retrieve"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework import generics
 
@@ -7,3 +8,18 @@ from user.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    """Update user witch already login"""
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    """Retrieve user witch already login"""
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
